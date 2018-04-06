906a7ea5a1da1071f9e348be865fb261206e4476
Azelor <kaarel.soot@gmail.com>
2018-04-06 16:12:38 +0300

    Wrap Person in Error Boundary

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import './Person.css';
 
-const person = (props) => {
-    const style = { 
-        '@media (min-width: 500px': {
-            width: '450px'
+import classes from './Person.css';
+
+const person = ( props ) => {
+    const rnd = Math.random();
+
+        if (rnd > 0.7) {
+            throw new Error('something went wrong')
         }
-    };
-
     return (
-    <div className="Person" style={style}>
-        <p onClick={props.click} >I'm {props.name} and I am {props.age} years old!</p>
-        <p>{props.children}</p>
-        <input type="text" onChange={props.changed} value={props.name}/>
-    </div>
+        
+
+        <div className={classes.Person}>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name} />
+        </div>
     )
-}; 
+};
 
 export default person;
